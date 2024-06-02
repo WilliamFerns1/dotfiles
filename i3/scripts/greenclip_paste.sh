b2bb492d80035adb6ba317a30dc5277e2f33556d
WilliamFerns1 <willfv2@gmail.com>
2024-06-02 18:10:53 +0200

    feat(screenshots): being able to take screenshots with command '$mod+p'

--- i3/scripts/greenclip_paste.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the selected item from Greenclip's history
-selected_item=$(greenclip print | rofi -dmenu -p "Clipboard" -width 1000)
-
-# Paste the selected item
-echo -n "$selected_item" | xclip -selection clipboard
-
