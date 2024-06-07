1fab000f74d6391d20a496c0896509b25768d03f
WilliamFerns1 <willfv2@gmail.com>
2024-06-07 21:55:55 +0200

    feat(fish): added aliases for git
    copied from brothers config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias n 'nvim'
+    alias n "nvim"
     alias lvim "/home/adonis/.local/bin/lvim"
     set -U fish_mode_cursor block
 
+    alias ll "ls -alF"
+
+# GitHub Aliases
+    alias gst "git status"
+    alias gcm "git commit -m"
+    alias gcz "git cz commit"
+    alias gaa "git add --all"
+    alias ga "git add"
+    alias gp "git push"
+    alias gpf "git push --force"
+    alias gpl "git pull"
+    alias gsm "git switch main"
+    alias gsmp "git switch main && git pull"
+    alias gcb "git checkout -b"
     # Add Homebrew to PATH
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     # > /dev/null 2>&1;
@@ -11,15 +25,6 @@ if status is-interactive
 
 function g
     switch $argv[1]
-        case "st"
-            git status
-            return
-        case "czc"
-            git cz commit
-            return
-        case "p"
-            git push
-            return
         case "*"
             git $argv
             return
